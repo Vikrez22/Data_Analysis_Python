c46d512a9bee4168aa53f27ad85438f623c84041
viktech <111059409+Vikrez22@users.noreply.github.com>
2023-06-27 14:20:04 +0100

    Analyzed_2018_Central_Park_Squirrel_Census

---
+++ main.py
@@ -0,0 +1,24 @@
+import pandas
+
+# get all the grey squirrels
+# data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# grey_squirrels = data[data["Primary Fur Color"] == "Gray"]
+# print(grey_squirrels)
+
+# count all squirrels
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+# constructing a data frame
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+dataFrame = pandas.DataFrame(data_dict)
+dataFrame.to_csv("squirrel_count.csv")
